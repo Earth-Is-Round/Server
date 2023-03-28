0038189fe2cb54865b054a2ac5403b368b3a1391
oh980225 <oh980225@gmail.com>
2023-03-28 21:23:36 +0900

    토큰의 만료기간은 30일

--- src/main/java/com/earth/server/user/domain/GenerateToken.java
+++ src/main/java/com/earth/server/user/domain/GenerateToken.java
@@ -1,7 +1,5 @@
 package com.earth.server.user.domain;
 
-import java.time.Instant;
-
 public interface GenerateToken {
-  Token makeToken(Long id, Instant expiration);
+  Token makeToken(Long id);
 }
--- src/main/java/com/earth/server/user/infra/security/jwt/JwtProvider.java
+++ src/main/java/com/earth/server/user/infra/security/jwt/JwtProvider.java
@@ -5,22 +5,24 @@ import com.earth.server.user.domain.Token;
 import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
 
-import java.sql.Date;
 import java.time.Instant;
+import java.time.Period;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 @RequiredArgsConstructor
 public class JwtProvider implements GenerateToken {
   private final JwtSecretKey secretKey;
+  private static final Period EXPIRATION_PERIOD = Period.ofDays(30);
 
   @Override
-  public Token makeToken(Long id, Instant expiration) {
+  public Token makeToken(Long id) {
     Map<String, Object> claims = new HashMap<>();
     claims.put("id", id);
     var accessToken = Jwts.builder()
       .setClaims(claims)
-      .setExpiration(Date.from(expiration))
+      .setExpiration(Date.from(Instant.now().plus(EXPIRATION_PERIOD)))
       .signWith(secretKey.get())
       .compact();
 
