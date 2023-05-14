33daab46f0a0b52c8cf11a8733d04b2d9275f957
oh980225 <oh980225@gmail.com>
2023-05-14 18:21:53 +0900

    빠진 트랜잭션 처리 추가

--- src/main/java/com/earth/server/user/domain/SignUpUseCase.java
+++ src/main/java/com/earth/server/user/domain/SignUpUseCase.java
@@ -4,12 +4,14 @@ import com.earth.server.common.domain.DomainException;
 import com.earth.server.common.domain.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 public class SignUpUseCase {
   private final UserRepository userRepository;
   private final PasswordEncoder passwordEncoder;
 
+  @Transactional
   public void run(Nickname nickname, Password password) {
     if (userRepository.find(nickname.value()).isPresent()) {
       throw new DomainException(ErrorCode.DUPLICATE_NICKNAME);
