be9f2e8157351988acaf008d17eb193aa3341824
oh980225 <oh980225@gmail.com>
2023-09-23 16:47:31 +0900

    기록 목록 조회 API 추가

--- src/main/java/com/earth/server/core/repository/SnowmanRepository.java
+++ src/main/java/com/earth/server/core/repository/SnowmanRepository.java
@@ -4,8 +4,11 @@ import com.earth.server.user.infra.persistence.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface SnowmanRepository extends JpaRepository<SnowmanEntity, Long> {
     Optional<SnowmanEntity> findByUserAndStartDate(UserEntity user, LocalDate startDate);
+    Long countByUserAndStartDateLessThanEqual(UserEntity user, LocalDate startDate);
+    List<SnowmanEntity> findFirst10ByUserAndStartDateLessThanEqual(UserEntity user, LocalDate startDate);
 }
